c3341bf72f4b46fb36ca178a083ebd521ddffed3
herman <herman@gademuu>
2013-12-23 10:06:10 +0700

    new commit

--- development/JavaQuest/src/backend/PropStudentDaoImpl.java
+++ development/JavaQuest/src/backend/PropStudentDaoImpl.java
@@ -5,7 +5,7 @@ package backend;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Properties;
+import java.util.*;
 
 /**
  *
@@ -13,31 +13,33 @@ import java.util.Properties;
  */
 class PropStudentDaoImpl extends PropDaoImpl implements StudentDao {
     
-    private static final String FILE = "student.properties";
     
-    public String create(Student aStudent){
+    private static final String STUDENT_FILE_PREFIX = "student_";
+    private static final String STUDENT_FILE_SUFFIX= ".properties";
+    
+    public Student create(Student aStudent){
         
         String id = null;       
         Properties prop = new Properties();
-        prop = load(FILE);
-           // prop.list(System.out);
+        prop = load(STUDENT_FILE_PREFIX + aStudent.getName() + STUDENT_FILE_SUFFIX);
+          
         
         prop.setProperty(aStudent.getName(), aStudent.getPassword());
-        store(FILE, prop, true);
+        store(STUDENT_FILE_PREFIX + aStudent.getName() + STUDENT_FILE_SUFFIX, prop, true);
         id = aStudent.getName();
            
-        return id;
+        return aStudent;
     
     }
 
-    public Student read(String id){
+    public Student read(Student aStudent){
     
         Student s = null;
         Properties prop = new Properties();
-        
-        prop = load(FILE);
-        prop.get(id);
-        s = new Student(id, prop.getProperty(id));
+        String id = aStudent.getName();
+        prop = load(STUDENT_FILE_PREFIX + aStudent.getName() + STUDENT_FILE_SUFFIX);
+        String name = (String)prop.get(id);
+        s = new Student(name, aStudent.getName());
         
         return s;
     
@@ -48,12 +50,24 @@ class PropStudentDaoImpl extends PropDaoImpl implements StudentDao {
  // TODO
  }
     
-    public void delete(String id) {
+    public void delete(String aStudent) {
     // TODO
     }
     
     public Student[] find(Student criteria) {
-        // TODO
+        
+        ArrayList<Student> matches = new ArrayList<>();
+        Properties prop = load(STUDENT_FILE_PREFIX + criteria.getPassword() + STUDENT_FILE_SUFFIX);
+        
+        Set<Map.Entry<Object, Object>> students;
+        students = prop.entrySet();
+        Iterator it = students.iterator();
+        
+        while(it.hasNext()){
+            Map.Entry<Object, Object> e = (Map.Entry<Object, Object>) it.next();
+            Student s = new Student((String)e.getKey(), criteria.getPassword());
+            matches.add(s);
+        }
         return null;
     }
     
